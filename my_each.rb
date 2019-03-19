39e7ab0f5e42a95cae17ed47f7312482af9b9a77
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-03-19 01:31:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,7 +3,7 @@ def my_each(array)
   if block_given?
     i = 0
     while i < array.length
-      yield(array)
+      yield(array[i])
       i = i + 1
     end
     array
@@ -14,5 +14,4 @@ end
 collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   puts i
-  array
 end
